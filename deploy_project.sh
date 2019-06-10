5841e42adebe805294b11b6458a0190e237d0c30
mitsuoysharag <mitsuoysharag@gmail.com>
2019-06-10 17:25:11 -0500

    deploy project

--- deploy_project.sh
+++ deploy_project.sh
@@ -5,6 +5,6 @@ git init
 git add .
 git commit -m 'deploy project'
 # git remote rm origin
-git remote add origin https://github.com/mitsuoysharag/TesisChatbot_Vue.git
+# git remote add origin https://github.com/mitsuoysharag/TesisChatbot_Vue.git
 # git pull origin master
 git push -u origin master
